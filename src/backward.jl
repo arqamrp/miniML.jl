38282e9a9aaf75201b440678b08b909c4b81dc98
Arqam Patel <62590607+arqamrp@users.noreply.github.com>
2023-11-20 13:46:03 +0530

    Add files via upload

---
+++ src/backward.jl
@@ -0,0 +1,109 @@
+# we go from matrix 1 (mxn) and 2 (nxp) to matrix 3 (mxp) through matmult
+# grad3 is mxp
+# grad1->3 is mxp x mxn
+# grad1 is mxn: elementwise mult of grad3
+
+function backward!(x::Tensor)
+
+    #UNARY OPS
+    # exp
+    if x._op == "exp"
+        if x._prev[1]._flag
+            x._prev[1].grad += x.data
+        end
+
+    # log
+    elseif x._op == "log"
+        if x._prev[1]._flag
+            x._prev[1].grad += 1 ./ (x._prev[1].data)
+        end
+    # transpose
+    elseif x._op == "T"
+        if x._prev[1]._flag
+            x._prev[1].grad += x.grad'
+        end
+    
+    elseif x._op == "getindex"
+        idx = x._prev[2]
+        if x._prev[1]._flag
+            x._prev[1].grad[idx...] += x.grad
+        end
+        
+    # reshaping and stuff
+    
+    elseif x._op == "reshape"
+        if x._prev[1]._flag
+            x._prev[1].grad += reshape(x.grad, x._prev[1].shape)
+        end
+    
+    # naive broadcast
+    elseif x._op == "broadcast"
+        if x._prev[1]._flag
+            x._prev[1].grad += sum(x.grad, dim = x._prev[3])
+        end
+    
+
+
+#     elseif x._op
+    
+    # MIXED TENSOR-SCALAR OPS
+    # ^
+    elseif x._op == "^"
+        if x._prev[1]._flag
+            k = x._prev[2]
+            x._prev[1].grad += (k) * (x._prev[1].data .^ (k-1))
+        end
+    
+    # Scalar Tensor addition
+    elseif x._op== "scalar+"
+        if x._prev[1]._flag
+            x._prev[1].grad += x.grad
+        end
+        
+    # Scalar tensor multiplication
+    elseif x._op== "scalar*"
+        if x._prev[1]._flag
+            x._prev[1].grad += x.grad * x._prev[2]
+        end
+        
+    # Scalar division (k/tensor)
+    elseif x._op == "/"
+        if x._prev[1]._flag
+            x._prev[1].grad += x._prev[2] ./ (x._prev[1].data .^ 2)
+        end
+        
+    # Scalar comparisons are assumed to have zero gradients
+    
+
+
+    #BINARY OPS
+    # Matrix multiplication
+    elseif x._op == "*"
+        if x._prev[1]._flag
+            x._prev[1].grad += x.grad * x._prev[2].data' # += since
+        end
+        if x._prev[2]._flag
+            x._prev[2].grad +=  x._prev[1].data' * x.grad
+        end
+        
+    # Tensor addition
+    elseif x._op == "+"
+        if x._prev[1]._flag
+            x._prev[1].grad += x.grad
+        end
+        if x._prev[2]._flag
+            x._prev[2].grad += x.grad
+        end
+    
+    # Elementwise multiplication
+    elseif x._op == "elemwise"
+        if x._prev[1]._flag
+            x._prev[1].grad += x._prev[2].data
+        end
+        if x._prev[2]._flag
+            x._prev[2].grad += x._prev[1].data
+        end
+        
+    end
+
+end
---
+++ src/interface.jl
@@ -0,0 +1,48 @@
+
+
+mutable struct tensor_function # for general scalar function definitions
+    _func::Function # should be a function of params and input
+    params::Vector{Tensor} # should be a array of tensors
+
+    function tensor_function(func::Function, input_params::Vector)
+        init_params = [Tensor(float.(param), true) for param in input_params]
+        new(func, init_params)
+    end
+end
+
+
+function forward(model::tensor_function, input::Vector)
+    if !isTensor(input[1])
+        input = [Tensor(float.(inp), false) for inp in input]
+    end            
+    return model._func(model.params, input)
+end
+
+
+function Backward!(loss::Tensor, graph_arr::Array{Tensor})
+    loss.grad = 1.
+    
+    function back(node::Tensor)
+        if node._flag & !isempty(node._prev) # if not leaf node or scalar
+            backward!(node)
+            back(node._prev[1])
+            back(node._prev[2])
+        end
+    end
+    
+    back(loss)
+        
+end
+
+
+function zerograd!(model::tensor_function)
+    for param in model.params
+        param.grad = zeros(param.dtype ,param.shape)
+    end
+end
+
+function train!(model::tensor_function)
+    for param in model.params
+        param._train = true 
+    end
+end
